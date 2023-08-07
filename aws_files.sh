8ed02838a2a7b255b72e0c1c43809335ce1a5878
gabst138@gmail.com <gabst138@gmail.com>
2023-08-07 11:07:35 -0300

    feat: first commit add script

---
+++ aws_files.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ "$1" == "" ]; then
+    echo "Mode de uso: $0 <bucket>"
+else
+    for path in $(cat path); do
+        aws s3 cp s3://$1/$path ./s3_files/ 1> /dev/null
+        echo "Arquivo Baixado: $path"
+    done
+    zip -r s3_files.zip s3_files/ 1> /dev/null
+    echo "Zip gerado: s3_files.zip"
+    read -p "Quer apagar os arquivos baixados? [y/n]: " -n 1 delete
+    echo ""
+    if [ "$delete" == "y" ]; then
+        for files in $(ls s3_files/); do
+            rm s3_files/$files 1> /dev/null
+            echo "Arquivo Deletado: $files"
+        done
+    fi
+fi
