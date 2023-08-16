17d3eb845ca3e262c5fca29f0a1f8624b66f26cd
gabst138@gmail.com <gabst138@gmail.com>
2023-08-16 14:26:53 -0300

    fix: arquivos com espaco nao baixa

--- aws_files.sh
+++ aws_files.sh
@@ -3,18 +3,31 @@
 if [ "$1" == "" ]; then
     echo "Modo de uso: $0 <bucket>"
 else
-    for path in $(cat path); do
-        aws s3 cp s3://$1/$path ./s3_files/ 1> /dev/null
+    totLine=$(wc -l path.txt | cut -d ' ' -f 1)
+    if [ $totLine == 0 ]; then
+        path=$(cat path.txt)
+        aws s3 cp "s3://$1/$path" ./s3_files/ 1> /dev/null
         echo "Arquivo Baixado: $path"
-    done
+    else
+        for path in $(cat path.txt); do
+            aws s3 cp "s3://$1/$path" ./s3_files/ 1> /dev/null
+            echo "Arquivo Baixado: $path"
+        done
+    fi
     zip -r s3_files.zip s3_files/ 1> /dev/null
     echo "Zip gerado: s3_files.zip"
     read -p "Quer apagar os arquivos baixados? [y/n]: " -n 1 delete
     echo ""
     if [ "$delete" == "y" ]; then
-        for files in $(ls s3_files/); do
-            rm s3_files/$files 1> /dev/null
+        if [ $totLine == 0 ]; then
+            files=$(ls s3_files/)
+            rm "s3_files/$files" 1> /dev/null
             echo "Arquivo Deletado: $files"
-        done
+        else
+            for files in $(ls s3_files/); do
+                rm s3_files/$files 1> /dev/null
+                echo "Arquivo Deletado: $files"
+            done
+        fi
     fi
 fi
